d6e41b48ab90199563ac24886f2db13ba7060791
Ana Novković <anaiscoding@gmail.com>
2024-08-03 10:29:45 +0200

    Update ChordSelector.js

--- src/components/ChordSelector.js
+++ src/components/ChordSelector.js
@@ -10,6 +10,18 @@ const ChordSelector = ({ onChordChange }) => {
       <select className="form-select" onChange={handleChange}>
         <option value="C">C Major</option>
         <option value="D">D Major</option>
+        <option value="E">E Major</option>
+        <option value="F">F Major</option>
+        <option value="G">G Major</option>
+        <option value="A">A Major</option>
+        <option value="B">B Major</option>
+        <option value="Cm">C Minor</option>
+        <option value="Dm">D Minor</option>
+        <option value="Em">E Minor</option>
+        <option value="Fm">F Minor</option>
+        <option value="Gm">G Minor</option>
+        <option value="Am">A Minor</option>
+        <option value="Bm">B Minor</option>
       </select>
     </div>
   );
